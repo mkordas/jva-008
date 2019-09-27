df5c787da01de762882ef74a62cbffb130a5d1db
PiterOfLuxoft <07dzwonki>
2019-09-27 09:49:51 +0200

    Adding space between field and a method.

--- src/test/java/com/lufoxt/training/jva008/Logger.java
+++ src/test/java/com/lufoxt/training/jva008/Logger.java
@@ -3,8 +3,9 @@ package com.lufoxt.training.jva008;
 
 public class Logger {
 
-    private static final org.apache.log4j.Logger LOGGER  =
-        org.apache.log4j.Logger.getLogger(Logger.class);
+    private static final org.apache.log4j.Logger LOGGER =
+            org.apache.log4j.Logger.getLogger(Logger.class);
+
     public static void log(String s) {
         LOGGER.info(s);
     }
